4ef8be5e3f1e8b93661900f5fc03537fe2786b58
Konrad Zemek <konrad.zemek@gmail.com>
2020-12-30 19:41:54 +0100

    Improve Outlaw rotation, add NightFae spells.

--- Specialization/Outlaw.lua
+++ Specialization/Outlaw.lua
@@ -16,6 +16,7 @@ local OL = {
 	GhostlyStrike        = 196937,
 	DeeperStratagem      = 193531,
 
+	-- Roll the Bones buffs
 	SkullAndCrossbones   = 199603,
 	TrueBearing          = 193359,
 	RuthlessPrecision    = 193357,
@@ -42,8 +43,16 @@ local OL = {
 
 	StealthAura          = 1784,
 	VanishAura           = 11327,
-	InstantPoison        = 315584
+	InstantPoison        = 315584,
+
+	-- Covenant Abilities
+	Sepsis               = 328305,
+	SepsisAura           = 347037
 };
+
+local Conduits = {
+	SleightOfHand = 244
+}
 
 local A = {
 	Deadshot        = 272935,
@@ -57,6 +66,10 @@ setmetatable(A, Rogue.spellMeta);
 
 local Rtb = { 'Broadside', 'GrandMelee', 'RuthlessPrecision', 'TrueBearing', 'SkullAndCrossbones', 'BuriedTreasure' };
 
+function Rogue:IsRefreshable(buff)
+	return buff.refreshable and buff.remains <= 10
+end
+
 function Rogue:Outlaw()
 	local fd = MaxDps.FrameData;
 	local cooldown = fd.cooldown;
@@ -64,6 +77,8 @@ function Rogue:Outlaw()
 	local buff = fd.buff;
 	local talents = fd.talents;
 	local timeToDie = fd.timeToDie;
+	local covenantId = fd.covenant.covenantId;
+	local conduit = fd.covenant.soulbindConduits;
 	local targets = MaxDps:SmartAoe();
 	local comboPoints = UnitPower('player', 4);
 	local comboPointsMax = UnitPowerMax('player', 4);
@@ -78,6 +93,14 @@ function Rogue:Outlaw()
 		comboGain = 1;
 	end
 
+	local rollTheBonesBuffCount = 0;
+	if buff[OL.SkullAndCrossbones].up then rollTheBonesBuffCount = rollTheBonesBuffCount + 1; end
+	if buff[OL.TrueBearing].up        then rollTheBonesBuffCount = rollTheBonesBuffCount + 1; end
+	if buff[OL.RuthlessPrecision].up  then rollTheBonesBuffCount = rollTheBonesBuffCount + 1; end
+	if buff[OL.GrandMelee].up         then rollTheBonesBuffCount = rollTheBonesBuffCount + 1; end
+	if buff[OL.BuriedTreasure].up     then rollTheBonesBuffCount = rollTheBonesBuffCount + 1; end
+	if buff[OL.Broadside].up          then rollTheBonesBuffCount = rollTheBonesBuffCount + 1; end
+
 	MaxDps:GlowEssences();
 	MaxDps:GlowCooldown(OL.AdrenalineRush, cooldown[OL.AdrenalineRush].ready);
 	--ADRENALINE RUSH KILLING SPREE BLADE RUSH MARKED FOR DEATH
@@ -86,43 +109,54 @@ function Rogue:Outlaw()
 	--	MaxDps:GlowCooldown(OL.AdrenalineRush);
 	--end
 
-	if cooldown[OL.RollTheBones].ready and energy >= 25 and not (buff[OL.Broadside].up or buff[OL.RuthlessPrecision].up) then
-		return OL.RollTheBones;
+	if buff[OL.StealthAura].up or buff[OL.VanishAura].up or buff[OL.SepsisAura].up then
+		return OL.Ambush;
 	end
 
-	if targets >= 2 and cooldown[OL.BladeFlurry].ready and buff[OL.BladeFlurry].refreshable then
+	if targets >= 2 and cooldown[OL.BladeFlurry].ready and Rogue:IsRefreshable(buff[OL.BladeFlurry]) then
 		return OL.BladeFlurry;
+	end
+
+	if cooldown[OL.RollTheBones].ready then
+		-- Always reroll single buffs if Sleight of Hand is your Potency Conduit
+		if conduit[Conduits.SleightOfHand] > 0 and rollTheBonesBuffCount < 2 then return OL.RollTheBones; end
+		-- No buffs or single buff if it's neither Broadside nor True Bearing
+		if rollTheBonesBuffCount < 2 and not buff[OL.Broadside].up and not buff[OL.TrueBearing].up then return OL.RollTheBones; end
+		-- Two buffs if it's Grand Melee AND Buried Treasure
+		if rollTheBonesBuffCount == 2 and buff[OL.GrandMelee].up and buff[OL.BuriedTreasure] then return OL.RollTheBones; end
+	end
+
+	if cooldown[OL.BetweenTheEyes].ready and comboPointsDeficit <= 1 then
+		return OL.BetweenTheEyes;
+	end
+
+	if cooldown[OL.SliceAndDice].ready and comboPointsDeficit <= 1 and Rogue:IsRefreshable(buff[OL.SliceAndDice]) then
+		return OL.SliceAndDice;
+	end
+
+	if talents[OL.BladeRush] and cooldown[OL.BladeRush].ready then
+		return OL.BladeRush;
 	end
 
 	if talents[OL.KillingSpree] and cooldown[OL.KillingSpree].ready and not buff[OL.AdrenalineRush].up then
 		return OL.KillingSpree;
 	end
-	if talents[OL.BladeRush] and cooldown[OL.BladeRush].ready then
-		return OL.BladeRush;
+
+	if comboPointsDeficit >= 1 and covenantId == Enum.CovenantType.NightFae and cooldown[OL.Sepsis].ready then
+		return OL.Sepsis;
 	end
-	if comboPointsDeficit >= comboGain and buff[OL.Opportunity].up then
-		return OL.PistolShot;
-	else
-		if comboPointsDeficit >= comboGain then
-			return OL.SinisterStrike;
-		end
-	end
-	if comboPointsDeficit <= 1 and cooldown[OL.SliceAndDice].ready and buff[OL.SliceAndDice].refreshable and energy >= 25 then
-		return OL.SliceAndDice;
-	end
-	if talents[OL.GhostlyStrike] and comboPointsDeficit >= comboGain and cooldown[OL.GhostlyStrike].ready and energy >= 30 then
-		return OL.GhostlyStrike;
-	end
-	if cooldown[OL.BetweenTheEyes].ready and comboPointsDeficit <= 1 and energy >= 25 then
-		return OL.BetweenTheEyes;
-	end
-	if comboPointsDeficit <= 1 and cooldown[OL.Dispatch].ready and energy >= 35 then
+
+	if comboPointsDeficit <= 1 and cooldown[OL.Dispatch].ready then
 		return OL.Dispatch;
 	end
-	if energy >= (energyMax - 45) then
-		return OL.SinisterStrike;
+
+	if comboPointsDeficit >= 2 and cooldown[OL.Vanish].ready then
+		return OL.Vanish;
 	end
-	if energy <= (energyMax - 45) and buff[OL.Opportunity].up then
+
+	if comboPointsDeficit >= 2 and buff[OL.Opportunity].up then
 		return OL.PistolShot;
 	end
+
+	return OL.SinisterStrike;
 end
